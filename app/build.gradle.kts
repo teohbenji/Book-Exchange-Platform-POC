5ce9d3126d7fb0d80b8f92dbb49f5d374311ce63
teohbenji <teohbenjamin1@gmail.com>
2024-04-29 21:49:36 +0800

    Added recycler view, and image fetching using Glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,6 @@ dependencies {
 
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.github.bumptech.glide:glide:4.14.2")
 }
--- app/src/main/java/com/example/bookplace/ListBookPage.java
+++ app/src/main/java/com/example/bookplace/ListBookPage.java
@@ -1,16 +1,34 @@
 package com.example.bookplace;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.example.bookplace.Model.GoogleBooksApi;
 
 public class ListBookPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_book_page);
+
+        Button buttonSearch = findViewById(R.id.buttonSearch);
+        ImageView imageViewSmallThumbnail = findViewById(R.id.imageViewSmallThumbnail);
+        String smallThumbnailUrl = "https://books.google.com/books/content?id=JHEkAQAAMAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api";
+        Glide.with(this).load(smallThumbnailUrl).into(imageViewSmallThumbnail);
+
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoogleBooksApi googleBooksApi = new GoogleBooksApi();
+                googleBooksApi.getData(getApplicationContext(), "Da Vinci code");
+            }
+        });
     }
+
+
 }
